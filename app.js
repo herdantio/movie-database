a8a042b1479f52b682867133db97578520e923c9
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-06 15:33:49 +0700

    add connect & clode db method

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 const baseRouter = require('./routes/index')
 
 var app = express();
---
+++ util/db.js
@@ -0,0 +1,28 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('seryu-db', 'postgres', 'docker', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+async function connectDB() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+async function closeDB() {
+    try {
+        await sequelize.close();
+        console.log('Connection has been closed successfully.');
+    } catch (error) {
+        console.error('Unable to close connection to the database:', error);
+    }
+}
+module.exports = {
+    connectDB,
+    closeDB
+}
