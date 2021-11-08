28291eabf3b09ab8a637b90bedfcfb30b0ea97d9
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-08 20:37:45 +0700

    generate review model & migration with paranoid mode

---
+++ migrations/20211108132112-create-review.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewName: {
+        type: Sequelize.STRING
+      },
+      reviewDesc: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addColumn('Reviews', 'deletedAt', {
+      allowNull: true,
+      type: Sequelize.DATE
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reviews');
+    await queryInterface.removeColumn('Reviews', 'deletedAt');
+  }
+};
