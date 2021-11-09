281c5faae5f08fde9ddc8577efb6afab880edfb0
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-09 14:33:50 +0700

    configure docker-compose to run either backend only of full service

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
-echo "build and run container"
-docker-compose up -d --build
-echo "finished building & running"
+if [[ $1 == "all" ]]
+then
+  printf 'Running All Docker Service \n'
+  echo "build and run container"
+  docker-compose up -d --build
+  echo "finished building & running"
+else
+    printf 'Running Only Backend Docker Service \n'
+  echo "build and run container"
+  docker-compose up -d --build server postgres
+  echo "finished building & running"
+fi
 
 echo "Migration started"
 docker exec server npm run migrate
