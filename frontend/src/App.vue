dde52f12789279c37dac588071814200198810a5
HERDANTIO <herdantio.sindhunata@binus.ac.id>
2021-11-09 13:56:46 +0700

    add menubar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,18 @@
 <template>
+  <menu-bar/>
   <router-view/>
 </template>
+
+<script>
+import {defineAsyncComponent} from "vue";
+
+export default {
+  name: "App",
+  components: {
+    MenuBar: defineAsyncComponent(() => import('@/atomic/molecules/MenuBar'))
+  }
+}
+</script>
 
 <style lang="scss">
 
---
+++ frontend/src/atomic/molecules/MenuBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-row justify="space-between" class="menuBarBackgroundColor paddingMenu">
+    <el-col :span="3" class="marginMenuItem">
+      <h2>Movie</h2>
+    </el-col>
+    <el-col :span="4" class="marginMenuItem">
+      <el-button round>ADD MOVIE</el-button>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+  name: "MenuBar"
+}
+</script>
+
+<style scoped>
+.menuBarBackgroundColor{
+  background-color: #5D5FEF;
+}
+.paddingMenu{
+  padding: 2px;
+}
+.marginMenuItem{
+  margin: 2px;
+}
+
+.badgeFont{
+  font-size: 20px;
+}
+</style>
